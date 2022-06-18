7018c97183f2386bafb35cf002a0cdf70d5c2402
Umer Nasir <umernasirr@gmail.com>
2022-06-18 19:33:16 +0500

    te,p

--- android/app/src/main/java/com/dtlapp/MainApplication.java
+++ android/app/src/main/java/com/dtlapp/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
+import com.reactnativecommunity.geolocation.GeolocationPackage;
 import com.clipsub.RNSweetAlert.RNSweetAlertPackage;
 import org.reactnative.camera.RNCameraPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
